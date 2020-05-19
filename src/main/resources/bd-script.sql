a36c6b628ad1909e86f22b16f15371542ee88536
Luiggi Mendoza <luiggi0925@gmail.com>
2020-05-18 19:33:14 -0500

    Added database script file

---
+++ src/main/resources/bd-script.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE encuestas
+GO
+USE encuestas
+GO
+CREATE TABLE dbo.encuesta (
+	id bigint PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	titulo nvarchar(50)
+)
+GO
+CREATE TABLE dbo.pregunta (
+	id bigint PRIMARY KEY IDENTITY(1,1),
+	texto nvarchar(100),
+	encuesta_id bigint NOT NULL,
+	FOREIGN KEY (encuesta_id) REFERENCES dbo.encuesta(id)
+)
+GO
